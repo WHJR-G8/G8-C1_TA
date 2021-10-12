ad03b831a2ec36bf6dcd0e1342bea1434eca333e
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-12 14:44:53 +0530

    Update Sol_Square_Without_Variable.py

--- Sol_Square_Without_Variable.py
+++ Sol_Square_Without_Variable.py
@@ -1,8 +1,12 @@
-import turtle
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
+import turtle #To import the module named "turtle"
+
+turtle.forward(50) #Make the turtle move in the forward direction by 50 pixels
+turtle.left(90) #Make the turtle turn towards its left side by 90 degrees
+
+turtle.forward(50) #Make the turtle move in the forward direction by 100 pixels
+turtle.left(90) #Make the turtle turn towards its left side by 90 degrees
+
+turtle.forward(50) #Make the turtle move in the forward direction by 100 pixels
+turtle.left(90) #Make the turtle turn towards its left side by 90 degrees
+
+turtle.forward(50) #Make the turtle move in the forward direction by 100 pixels
